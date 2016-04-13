f04b6477fb938fda9426713e5dc68e9e0906149f
Prelang Builder <builder@prelang.com>
2016-04-13 22:36:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Collection' to allow for create, update

--- app/admin/collection.rb
+++ app/admin/collection.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Collection do
+  permit_params :title, :description, :sold_count, :shirt_names, :url
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
